dd237016bec94601e8ab2a217caf4ab53583aa82
Shruti Vaidya <vaidya.shruti1108@gmail.com>
2022-02-28 00:28:29 +0545

    modified form dropdown added

--- src/App.js
+++ src/App.js
@@ -4,25 +4,119 @@ import FormInput from './components/FormInput/FormInput';
 
 function App() {
 
-  const [username, setUsername] = useState('')
+  const [values, setValues] = useState({
+    username:'',
+    email:'',
+    number:'',
+    DOB:'',
+    city:'',
+    district:'',
+    country:'Nepal',
+    province:''
+  });
+
+  const inputs = [
+    {
+      id:1,
+      name:'username',
+      label:'User name',
+      placeholder:'username',
+      errorMessage:'name an be 20 characters long and should not contain any special characters.',
+      type:'text',
+      required: true,
+    },
+    {
+      id:2,
+      name:'email',
+      label:'Email',
+      placeholder:'email',
+      type:'email',
+      errorMessage:'It should be a valid email.',
+      required: true,
+    },
+    {
+      id:3,
+      name:'phonenumber',
+      label:'Phone Number',
+      placeholder:'int:1234',
+      type:'text',
+      errorMessage:'7 characters long integer',
+      required: true,
+    },
+    {
+      id:4,
+      name:'DOB',
+      label:'DOB',
+      placeholder:'DOB',
+      type:'date',
+      required: true,
+    },
+    {
+      id:5,
+      name:'city',
+      label:'city',
+      placeholder:'city',
+      type:'text',
+      required: true,
+    },
+    {
+      id:6,
+      name:'district',
+      label:'district',
+      placeholder:'district',
+      type:'text',
+      required: true,
+    },
+    {
+      id:7,
+      name:'country',
+      label:'country',
+      type:'text',
+      required: true,
+    },
+    {
+      id:8,
+      name:'province',
+      label:'province',
+      placeholder:'province',
+      required: true,
+    }
+  ]
   
 
-  const submitHandler =(e) =>{
+  const submitHandler = (e) =>{
     e.preventDefault();
-    console.log(username);
-  }
+  };
+
+  const onChange= (e) =>{
+    setValues({...values, [e.target.name]:e.target.value });
+  };
+
+  console.log(values);
+  
   return (
     <div className="App">
-      <h2>React Form</h2>
-      <div className='box'>
-          <form onSubmit={submitHandler}> 
-              <FormInput placeholder='username' name='User name' setUsername={setUsername}/>
-              <FormInput placeholder='email'name='Email'/>
-              <FormInput placeholder='phone number' name='Phone Number:'/>
-              <FormInput placeholder='DOB' name='Date of Birth'/>
+      <h2>React form</h2>
+        <div className='wrapper'>
+        <form onSubmit={submitHandler}> 
+        <div className='box'>
+            {inputs.map((input) =>(
+              
+              <FormInput 
+                key={input.id} 
+                {...input} 
+                value={values[input.name]} 
+                onChange={onChange}
+              />
+          ))}
+        </div>
+            
+              
               <button>submit</button>
             </form>
-      </div>
+            </div>
+          
+      
         
     </div>
   );
--- src/components/FormInput/FormInput.js
+++ src/components/FormInput/FormInput.js
@@ -3,15 +3,25 @@ import './forminput.css';
 
 
 const FormInput = (props) => {
-
+  const { label, errorMessage, onChange, id, ...inputProps } = props;
  
   
   return (
     <div className='form-input'>
-        
-            <label >{props.name}</label>
-            <input placeholder={props.placeholder} onChange={e=>props.setUsername(e.target.value)}></input>
-            
+       
+            <label >{label}</label>
+            {id===8 ? 
+            <select className='dropdown'>
+              <option value='province1'>Province 1</option>
+              <option value='province2'>Province 2</option>
+              <option value='province3'>Province 3</option>
+              <option value='province4'>Province 4</option>
+              <option value='province5'>Province 5</option>
+              <option value='province6'>Province 6</option>
+            </select> :
+             <input {...inputProps} onChange={onChange}></input> }
+           
+            <span>{errorMessage}</span>
         
     </div>
   )
